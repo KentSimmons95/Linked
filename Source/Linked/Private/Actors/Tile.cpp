ec1c15c197abf89fed93678f071920ff792df54e
Kent Simmons <kent1372@gmail.com>
2022-08-02 15:43:36 +1200

    Initial Commit/Push

--- Source/Linked/Private/Actors/Tile.cpp
+++ Source/Linked/Private/Actors/Tile.cpp
@@ -9,6 +9,7 @@ ATile::ATile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
 }
 
 // Called when the game starts or when spawned
